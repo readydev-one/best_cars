d3f5fcbcd9e339e23f175dfcbdd733c63f4a695b
readydev-one <toricamstudios@gmail.com>
2025-06-14 12:20:57 -0400

    carModel development

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,67 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    # Other fields as needed
+
+    def __str__(self):
+        return self.name  # Return the name as the string representation
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from datetime import datetime
+
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('COUPE', 'Coupe'),
+        ('CONVERTIBLE', 'Convertible'),
+        ('HATCHBACK', 'Hatchback'),
+    ]
+    type = models.CharField(max_length=15, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(
+        default=datetime.now().year,
+        validators=[MinValueValidator(2015), MaxValueValidator(datetime.now().year)]
+    )
+    
+    # Fields to manage import tracking
+    is_imported = models.BooleanField(default=False)
+    last_synced = models.DateTimeField(null=True, blank=True)
+
+    # You might also want an external ID for reference
+    external_id = models.CharField(max_length=100, blank=True, null=True, unique=True)
+
+    fuel_efficiency = models.DecimalField(
+        max_digits=5, decimal_places=2, 
+        blank=True, null=True,
+        help_text="Fuel efficiency (e.g., miles per gallon or liters per 100km)"
+    )
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2,
+        blank=True, null=True,
+        help_text="Listing price of the car"
+    )
+    image_url = models.URLField(
+        max_length=500,
+        blank=True, null=True,
+        help_text="URL for the car image"
+    )
+    
+    def __str__(self):
+        return f"{self.car_make.name} {self.name} ({self.year})"
+
